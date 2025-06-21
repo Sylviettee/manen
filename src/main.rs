8e9fbcc19ef311f024c81c7ed7eda14c6511154f
Kate the Gay <kanako@gayest.dev>
2025-06-21 16:34:13 -0700

    Add highlight command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,54 @@
+use std::{
+    fs,
+    io::{self, Read},
+    path::PathBuf,
+};
+
+use clap::{Parser, Subcommand};
 use editor::Editor;
+use highlight::LuaHighlighter;
+use reedline::Highlighter;
 
 mod editor;
 mod format;
 mod highlight;
 mod validator;
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Repl,
+    Highlight { path: Option<PathBuf> },
+}
+
 fn main() -> color_eyre::Result<()> {
-    Editor::new()?.run();
+    let cli = Cli::parse();
+
+    match &cli.command {
+        None | Some(Command::Repl) => Editor::new()?.run(),
+        Some(Command::Highlight { path }) => {
+            let file = if let Some(path) = path {
+                fs::read_to_string(path)?
+            } else {
+                let mut buffer = String::new();
+                io::stdin().read_to_string(&mut buffer)?;
+
+                buffer
+            };
+
+            let highlighter = LuaHighlighter::new();
+            let text = highlighter.highlight(&file, 0);
+
+            println!("{}", text.render_simple());
+        }
+    }
+
     Ok(())
 }
