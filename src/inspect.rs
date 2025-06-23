2a9d825c2bd9c922ec27a26daa08b17310ce43e7
Kate the Gay <kanako@gayest.dev>
2025-06-22 18:19:22 -0700

    Reuse AhoCorasick

--- src/inspect.rs
+++ src/inspect.rs
@@ -1,38 +1,45 @@
 use aho_corasick::AhoCorasick;
+use lazy_static::lazy_static;
 use mlua::prelude::*;
 
+lazy_static! {
+    static ref AC_REPLACEMENTS: (AhoCorasick, Vec<String>) = {
+        let mut escapes = vec![
+            String::from("\x07"),
+            String::from("\x08"),
+            String::from("\x0C"),
+            String::from("\n"),
+            String::from("\r"),
+            String::from("\t"),
+            String::from("\x0B"),
+            String::from("\x7F"),
+        ];
+
+        let mut replacements = vec![
+            String::from("\\a"),
+            String::from("\\b"),
+            String::from("\\f"),
+            String::from("\\n"),
+            String::from("\\r"),
+            String::from("\\t"),
+            String::from("\\v"),
+            String::from("\\127"),
+        ];
+
+        for i in 0..=31 {
+            escapes.push(String::from_utf8_lossy(&[i]).to_string());
+            replacements.push(format!("\\{i}"));
+        }
+
+        (AhoCorasick::new(escapes).unwrap(), replacements)
+    };
+    static ref ESCAPER: &'static AhoCorasick = &AC_REPLACEMENTS.0;
+}
+
 fn escape_control(s: &str) -> String {
-    let mut escapes = vec![
-        String::from("\x07"),
-        String::from("\x08"),
-        String::from("\x0C"),
-        String::from("\n"),
-        String::from("\r"),
-        String::from("\t"),
-        String::from("\x0B"),
-        String::from("\x7F"),
-    ];
-
-    let mut replacements = vec![
-        String::from("\\a"),
-        String::from("\\b"),
-        String::from("\\f"),
-        String::from("\\n"),
-        String::from("\\r"),
-        String::from("\\t"),
-        String::from("\\v"),
-        String::from("\\127"),
-    ];
-
-    for i in 0..=31 {
-        escapes.push(String::from_utf8_lossy(&[i]).to_string());
-        replacements.push(format!("\\{i}"));
-    }
-
-    // TODO; reuse
-    let ac = AhoCorasick::new(escapes).unwrap();
-
-    ac.replace_all(s, &replacements).replace("\\\\x", "\\x")
+    ESCAPER
+        .replace_all(s, &AC_REPLACEMENTS.1)
+        .replace("\\\\x", "\\x")
 }
 
 fn remove_invalid(mut bytes: &[u8]) -> String {
